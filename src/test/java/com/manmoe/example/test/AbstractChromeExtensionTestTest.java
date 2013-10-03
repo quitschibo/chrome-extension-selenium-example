527827a53d3180f284dbd184e31355c85e460ab1
Manuel Möhlmann <mail@manmoe.com>
2013-10-03 13:41:30 +0200

    added new test

--- src/test/java/com/manmoe/example/test/AbstractChromeExtensionTestTest.java
+++ src/test/java/com/manmoe/example/test/AbstractChromeExtensionTestTest.java
@@ -1,7 +1,9 @@
 package com.manmoe.example.test;
 
 import com.manmoe.example.config.RemoteDriverConfig;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.mockito.Mockito.mock;
@@ -14,24 +16,49 @@ import static org.testng.Assert.assertEquals;
  * @author Manuel Möhlmann <mail@manmoe.com>
  */
 public class AbstractChromeExtensionTestTest {
+	private RemoteDriverConfig remoteDriverConfig;
+	private RemoteWebDriver remoteWebDriver;
+
+	private AbstractChromeExtensionTest testObject;
+
+	/**
+	 * Method for setting up the test environment.
+	 */
+	@BeforeMethod
+	public void setUp() {
+		remoteDriverConfig = mock(RemoteDriverConfig.class);
+		remoteWebDriver = mock(RemoteWebDriver.class);
+
+		testObject = new FirespottingIT();
+
+		// we insert our testmock in testObject
+		testObject.remoteDriverConfig = remoteDriverConfig;
+	}
 
 	/**
 	 * Tests, if we get a remoteDriver. We use the firespotting child class for testing.
 	 */
 	@Test
 	public void testGetWebDriverTest() {
-		RemoteDriverConfig remoteDriverConfig = mock(RemoteDriverConfig.class);
-		RemoteWebDriver webDriver = mock(RemoteWebDriver.class);
-
-		when(remoteDriverConfig.buildRemoteDriver()).thenReturn(webDriver);
-
-		AbstractChromeExtensionTest testObject = new FirespottingIT();
-		 // we insert our testmock in testObject
-		testObject.remoteDriverConfig = remoteDriverConfig;
+		when(remoteDriverConfig.buildRemoteDriver()).thenReturn(remoteWebDriver);
 
 		// run test
 		RemoteWebDriver result = testObject.getWebDriver();
 
-		assertEquals(result, webDriver);
+		assertEquals(result, remoteWebDriver);
+	}
+
+	/**
+	 * Tests the creation of the local driver.
+	 */
+	@Test
+	public void testGetLocalDriver() {
+		ChromeDriver chromeDriver = mock(ChromeDriver.class);
+		when(remoteDriverConfig.buildLocalDriver()).thenReturn(chromeDriver);
+
+		// run test
+		RemoteWebDriver result = testObject.getLocalDriver();
+
+		assertEquals(result, chromeDriver);
 	}
 }
