d6c7308cffda2652dc99f7228d6c9a609c006373
Manuel Möhlmann <mail@manmoe.com>
2013-10-22 22:38:49 +0200

    added new tests

--- src/test/java/com/manmoe/example/model/OptionsPageTest.java
+++ src/test/java/com/manmoe/example/model/OptionsPageTest.java
@@ -1,12 +1,15 @@
 package com.manmoe.example.model;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Tests the OptionsPage model.
@@ -54,5 +57,43 @@ public class OptionsPageTest {
 		optionsPage.close();
 
 		verify(webDriver, atLeastOnce()).close();
+	}
+
+	@Test
+	public void testGetRequestInterval() {
+		String expectedAttribute = "1000";
+
+		Select selectMock = mock(Select.class);
+		WebElement firstSelectedOptionMock = mock(WebElement.class);
+		WebElement requestIntervalMock = mock(WebElement.class);
+
+		doReturn(requestIntervalMock).when(webDriver).findElementById("RequestInterval");
+		doReturn(selectMock).when(optionsPage).createSelectByWebElement(requestIntervalMock);
+		when(selectMock.getFirstSelectedOption()).thenReturn(firstSelectedOptionMock);
+		when(firstSelectedOptionMock.getAttribute("value")).thenReturn(expectedAttribute);
+
+		// run test method
+		String result = optionsPage.getRequestInterval();
+
+		assertEquals(result, expectedAttribute);
+	}
+
+	@Test
+	public void testGetNotificationTimeout() {
+		String expectedAttribute = "1000";
+
+		Select selectMock = mock(Select.class);
+		WebElement firstSelectedOptionMock = mock(WebElement.class);
+		WebElement notificationTimeoutMock = mock(WebElement.class);
+
+		doReturn(notificationTimeoutMock).when(webDriver).findElementById("NotificationTimeout");
+		doReturn(selectMock).when(optionsPage).createSelectByWebElement(notificationTimeoutMock);
+		when(selectMock.getFirstSelectedOption()).thenReturn(firstSelectedOptionMock);
+		when(firstSelectedOptionMock.getAttribute("value")).thenReturn(expectedAttribute);
+
+		// run test method
+		String result = optionsPage.getNotificationTimeout();
+
+		assertEquals(result, expectedAttribute);
 	}
 }
